ee131a107e9b74c40650121d37709893340663fd
Lizet Lopez <lizet.chlopez@gmail.com>
2026-09-17 20:46:56 +0000

    Add src files and main sql

---
+++ sql/01_master_procedure.sql
@@ -0,0 +1,12 @@
+
+-- MASTER PROCEDURE
+-- Aquí se llaman todos los procedimientos de transformación de datos para cada tabla
+
+CREATE OR REPLACE PROCEDURE automation.sp_transform_all()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    CALL automation.sp_transform_orders_medium();
+    -- TODO: Agregar otros procedimientos de transformación aquí
+END;
+$$;
